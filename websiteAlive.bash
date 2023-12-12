7d250a583fb3df26fae4b7d6e759b6f6841487f6
Andrew <andrew@coulter.info>
2023-12-12 09:40:59 -0500

    changed settings to use email group. Added sending email to the right place in the script. Set var uuid so it should fail.

--- websiteAlive.bash
+++ websiteAlive.bash
@@ -12,16 +12,19 @@ apiOutput=$path/apiOutput.txt
 #Hit the cb api to build DManpa
 >$apiOutput #ensure this is empty
 python3 $path/api-https.py $website $path/build_sequence.json > $apiOutput
+# uncomment line below for testing emails.
+uuidInResponse="000"
 # If this sequence ID exists in the output, grpc etc is working
 if grep -q "$uuidInResponse" $apiOutput
 then
     #toDo # kick the watchdog
-    echo "$(date) : $website is fine" >> $activityLog    
+    echo "$(date) : $website is fine, uuid used: $uuidInResponse" >> $activityLog    
 else
     if [ "$(ping -c 1 $pingWebsite)" ]; then
         echo "$(date) : Both $pingWebsite and $website are unreachable, likely your internet is down dog" >> $activityLog
     else
         echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests, send email to $emailTo using this email server $emailServer" >> $activityLog
-        #toDo # send the email
+        #send the email
+	echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests." | mail --subject="$website api not responding correctly" $emailTo
     fi
 fi
