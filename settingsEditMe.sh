e376cbb7e6a5be778ca47af401531866c4745b3d
Oliver <olgrant@uga.edu>
2023-12-14 13:21:18 +0100

    Failure uuid used by Andrew was present in the response so never failed. Added one that fails to the settingsEditMe.sh that you can toggle on. Also when it failed and was able to ping google it gave the wrong message, so I swapped those to be correct. Tested with the false uuid and by turning off my internet. Everything works but I'm not able to know if emails actually send or not, over to Andrew.

--- settingsEditMe.sh
+++ settingsEditMe.sh
@@ -2,4 +2,5 @@ emailTo=glycam-alerts@googlegroups.com
 emailServer=na
 website=glycam.org
 pingWebsite=google.com
-uuidInResponse=fc6085c0-822c-5655-b5be-88da496814cb
+#uuidInResponse=fc6085c0-822c-IwIlLfAiLfOrAnDreW # This one fails. Useful for testing.
+uuidInResponse=fc6085c0-822c-5655-b5be-88da496814cb # This one works
--- websiteAlive.bash
+++ websiteAlive.bash
@@ -8,12 +8,9 @@ path=$1
 source $path/settingsEditMe.sh
 activityLog=$path/activity.log
 apiOutput=$path/apiOutput.txt
-
 #Hit the cb api to build DManpa
->$apiOutput #ensure this is empty
+>$apiOutput #ensure this is empty. Note the next line doesn't always overwrite.
 python3 $path/api-https.py $website $path/build_sequence.json > $apiOutput
-# uncomment line below for testing emails.
-uuidInResponse="000"
 # If this sequence ID exists in the output, grpc etc is working
 if grep -q "$uuidInResponse" $apiOutput
 then
@@ -21,10 +18,10 @@ then
     echo "$(date) : $website is fine, uuid used: $uuidInResponse" >> $activityLog    
 else
     if [ "$(ping -c 1 $pingWebsite)" ]; then
-        echo "$(date) : Both $pingWebsite and $website are unreachable, likely your internet is down dog" >> $activityLog
-    else
-        echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests, send email to $emailTo using this email server $emailServer" >> $activityLog
+        echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests, sending email to $emailTo using this email server $emailServer." >> $activityLog
         #send the email
-	echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests." | mail --subject="$website api not responding correctly" $emailTo
+	    echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests." | mail --subject="$website api not responding correctly." $emailTo
+    else
+        echo "$(date) : Both $pingWebsite and $website are unreachable, likely your internet is down dog." >> $activityLog
     fi
 fi
