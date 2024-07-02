7f6c775fbf337437546f52bd7c84daf809d0c6d8
gitoliver <olgrant@uga.edu>
2024-07-02 17:26:46 +0200

    Logic error when pinging google

--- websiteAlive.bash
+++ websiteAlive.bash
@@ -11,14 +11,14 @@ apiOutput=$path/apiOutput.txt
 
 #Hit the cb api to build DManpa
 >$apiOutput #ensure this is empty
-python3 $path/api-https.py $website $path/build_sequence.json > $apiOutput
+python3 $path/api-https.py $website $path/build_sequence.json &> $apiOutput
 # If this sequence ID exists in the output, grpc etc is working
 if grep -q "$uuidInResponse" $apiOutput
 then
     #toDo # kick the watchdog
     echo "$(date) : $website is fine" >> $activityLog    
 else
-    if [ "$(ping -c 1 $pingWebsite)" ]; then
+    if [ ! "$(ping -c 1 $pingWebsite)" ]; then
         echo "$(date) : Both $pingWebsite and $website are unreachable, likely your internet is down dog" >> $activityLog
     else
         echo "$(date) : $pingWebsite is ping-able but $website isn't responding normally to api requests, send email to $emailTo using this email server $emailServer" >> $activityLog
